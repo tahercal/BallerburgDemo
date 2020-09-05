7d7c4e003eccda1a7df2aa6650999335e86d8384
Taher Calcuttawala <taher@redappletech.com>
2020-09-05 19:19:27 +0530

    Cannon and Ball added

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [Header("References")]
+    public SpriteRenderer ballSprite;
+    public Rigidbody2D rigidbody;
+
+    [Header("Parameters")]
+    public float force;
+    public float angle;
+
+
+    private Vector2 direction;
+
+    private void Start()
+    {
+        direction = new Vector2(0f, angle);
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(direction * Time.deltaTime, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    [Header("Parameters")]
+    [Range(25,75)]
+    public float angle;
+    public float force;
+
+    [Header("References")]
+    [SerializeField]
+    private Transform cannon_Body;
+    [SerializeField]
+    private Transform ball_SpawnPoint;
+
+    [Header("Prefabs")]
+    public GameObject cannonBall;
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+
+            GameObject ballSpawned = Instantiate(cannonBall, ball_SpawnPoint);
+            ballSpawned.GetComponent<BallMovement>().force = this.force;
+            ballSpawned.GetComponent<BallMovement>().angle = this.angle;
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        if (cannon_Body != null)
+        {
+            cannon_Body.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+    }
+}
